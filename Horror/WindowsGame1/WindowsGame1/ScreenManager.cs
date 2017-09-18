055d5d0fe1f22ba0dbf5e1c59a19ed7f0dbf25bb
SeviceMikami <7sevice7@gmail.com>
2017-09-18 12:44:52 -0400

    Add files via upload

---
+++ Horror/WindowsGame1/WindowsGame1/ScreenManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace WindowsGame1
+{
+    public class ScreenManager
+    {
+        public Vector2 Dimensions { private set; get; } //Resolution
+        public ContentManager Content { private set; get; }
+        GameScreen CurrentScreen;
+        float ElapsedTime; //Used to calculat how long the SplashScreen is displayed to switch to MainMenu
+        float Delay = 2250f;
+        string Screen { private set; get; }
+
+        public ScreenManager()
+        {
+            Dimensions = new Vector2(1024, 768);
+            CurrentScreen = new SplashScreen();
+            Screen = "SplashScreen";
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+            this.Content = new ContentManager(Content.ServiceProvider, "Content");
+            CurrentScreen.LoadContent();
+        }
+
+        public void UnloadContent()
+        {
+            CurrentScreen.UnloadContent();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            CurrentScreen.Draw(spriteBatch);
+        }
+
+    }
+}
---
+++ Horror/WindowsGame1/WindowsGame1/SplashScreen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace WindowsGame1
+{
+    class SplashScreen : GameScreen
+    {
+        Texture2D splashScreen;
+        string Path;
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            Path = "GameScreens/SplashScreen";
+            splashScreen = content.Load<Texture2D>(Path);
+        }
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+            content.Unload();
+        }
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(splashScreen, Vector2.Zero, Color.White);
+        }
+    }
+}
