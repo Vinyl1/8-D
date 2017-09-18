907b6fdf12fa8f072fe7c0d7aaa9f8e395bf73ab
Anthony Conner <aconner7202@vbtc.net>
2017-09-18 12:43:20 -0400

    Herro

---
+++ Horror/WindowsGame1/WindowsGame1/GameScreen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace WindowsGame1
+{
+    class GameScreen
+    {
+
+        public virtual void LoadContent()
+        {
+
+        }
+        public virtual void UnloadContent()
+        {
+            
+        }
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
